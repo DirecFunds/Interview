8f595712bef60a26dd2cea9946467ca25414fca4
Marissa Sherman <marissa.sherman@direcfunds.com>
2025-02-03 11:16:59 -0500

    Initial commit

---
+++ TestGround/Program.cs
@@ -0,0 +1,9 @@
+﻿// In mathematics, the Fibonacci sequence is a sequence in which each element is the sum of the two elements that precede it.
+// Numbers that are part of the Fibonacci sequence are known as Fibonacci numbers, commonly denoted Fn .
+// Many writers begin the sequence with 0 and 1, although some authors start it from 1 and 1[1][2] and some (as did Fibonacci) from 1 and 2.
+//
+//  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+//
+// Please write a function that prints the first 10 numbers of the Fibonacci sequence starting at 0, 1
+
+Console.WriteLine("Hello, World!");
