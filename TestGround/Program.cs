d73f1078120884bf53cdf578f2d9637656088548
Marissa Sherman <marissa.sherman@direcfunds.com>
2025-02-06 08:30:28 -0500

    Update

--- TestGround/Program.cs
+++ TestGround/Program.cs
@@ -2,8 +2,8 @@
 // Numbers that are part of the Fibonacci sequence are known as Fibonacci numbers, commonly denoted Fn .
 // Many writers begin the sequence with 0 and 1, although some authors start it from 1 and 1[1][2] and some (as did Fibonacci) from 1 and 2.
 //
-//  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+//  1, 1, 2, 3, 5, 8, 13, 21, 34, 55
 //
-// Please write a function that prints the first 10 numbers of the Fibonacci sequence starting at 0, 1
+// Please write a function that prints the Nth number of the Fibonacci Sequence.
 
 Console.WriteLine("Hello, World!");
