d642ce6ea4e787330cbf40415215de108e073eb0
Marissa Sherman <marissa.sherman@direcfunds.com>
2025-11-12 14:39:55 -0500

    Add more problems

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,9 @@
+﻿//Listen to the prompt and follow the instructions you may
+//make any notes you would like as comments here at the top.
+
+FizzBuzz();
+
+void FizzBuzz()
+{
+    Console.WriteLine("Hello World");
+}
---
+++ ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿//Listen to the prompt and follow the instructions you may
+//make any notes you would like as comments here at the top.
+
+var inputString = "12345";
+
+var reversedString = ReverseString(inputString);
+
+Console.WriteLine($"Input String: {inputString}");
+Console.WriteLine($"Reversed String: {reversedString}");
+
+string ReverseString(string strToReverse)
+{
+    return String.Empty;
+}
+
